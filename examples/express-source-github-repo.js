e49fa6d656eef9a0224c00859735d69a4a1dfdd5
smapira <info@routeflags.com>
2017-09-04 08:22:19 +0900

    :arrow_up: Add some endpoints

--- examples/express-source-github-repo.js
+++ examples/express-source-github-repo.js
@@ -9,35 +9,70 @@ var sourceGithubGist = require('../lib/plugins/sourceGithubGist');
 var spawn = require('../lib/plugins/spawn');
 var sourceGithubRepo = require('../lib/plugins/sourceGithubRepo');
 
-var handler = spawn({
-  // code: nodeService,
-  language: "python"
-});
-
 app.use(logger());
 
+// hello worlds
+var lang = "python";
+app.get('/' + lang, sourceGithubRepo({
+  token: process.env.GIT_TOKEN,
+  repo: "smapira/microcule-examples",
+  branch: "master",
+  main: lang + "/index.py"
+}));
+app.get('/' + lang, bodyParser());
+app.get('/' + lang, spawn({
+  language: lang
+}));
 
-// source from github repo
-app.use(sourceGithubRepo({
+lang = "ruby";
+app.get('/' + lang, sourceGithubRepo({
+  token: process.env.GIT_TOKEN,
+  repo: "smapira/microcule-examples",
+  branch: "master",
+  main: lang + "/index.rb"
+}));
+app.get('/' + lang, bodyParser());
+app.get('/' + lang, spawn({
+  language: lang
+}));
+
+lang = "ocaml";
+app.get('/' + lang, sourceGithubRepo({
+  token: process.env.GIT_TOKEN,
+  repo: "smapira/microcule-examples",
+  branch: "master",
+  main: lang + "/index.ml"
+}));
+app.get('/' + lang, bodyParser());
+app.get('/' + lang, spawn({
+  language: lang
+}));
+
+lang = "tensorflow";
+app.get('/' + lang, sourceGithubRepo({
   token: process.env.GIT_TOKEN,
   repo: "smapira/microcule-examples",
   branch: "master",
   main: "python3-tensorflow/mnist_softmax.py"
 }));
-
-app.use(bodyParser());
-app.use(mschema({
-  "hello": {
-    "type": "string",
-    "required": true
-  }
+app.get('/' + lang, bodyParser());
+app.get('/' + lang, spawn({
+  language: "python"
 }));
-app.use(handler);
-app.use(function(req, res, next){
+
+var rust_service = {
+  language: 'rust',
+  code: require('fs').readFileSync(__dirname + '/services/hello-world/hello.rs').toString()
+};
+spawn = microcule.plugins.spawn(rust_service);
+app.get('/' + rust_service.language, function(req, res, next){
+  spawn(req, res, next);
+});
+
+app.use(function (req, res, next) {
   console.log('post process service');
   res.end();
 });
-
 app.listen(3000, function () {
   console.log('server started on port 3000');
 });
--- examples/services/echo/echo.py
+++ examples/services/echo/echo.py
@@ -4,5 +4,3 @@ print "Hook['params'] is populated with request parameters"
 pprint.pprint(Hook['params'])
 print "Hook['env'] is populated with env variables"
 pprint.pprint(Hook['env'])
-log('hello logs')
-log(Hook['params'])
